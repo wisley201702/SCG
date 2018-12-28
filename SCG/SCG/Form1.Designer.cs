3cae6044ded82c4ac2a478085b913d5128eda96f
wisley rodrigues <wisley.estacio2017@gmail.com>
2018-12-27 23:08:32 -0300

    CRIAÇÃO DOS CAMPOS E DOS BOTOES DA TELA DE LOGIN

--- SCG/SCG/Form1.Designer.cs
+++ SCG/SCG/Form1.Designer.cs
@@ -29,22 +29,128 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.senha = new System.Windows.Forms.TextBox();
+            this.usuario = new System.Windows.Forms.ComboBox();
+            this.ok = new System.Windows.Forms.Button();
+            this.cancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SCG.Properties.Resources.TELA_DE_LOGIN1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(150, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(127, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Usuario: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(139, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Senha:";
+            // 
+            // senha
+            // 
+            this.senha.Location = new System.Drawing.Point(211, 107);
+            this.senha.Name = "senha";
+            this.senha.Size = new System.Drawing.Size(153, 20);
+            this.senha.TabIndex = 2;
+            this.senha.UseSystemPasswordChar = true;
+            // 
+            // usuario
+            // 
+            this.usuario.FormattingEnabled = true;
+            this.usuario.Location = new System.Drawing.Point(211, 71);
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(153, 21);
+            this.usuario.TabIndex = 1;
+            // 
+            // ok
+            // 
+            this.ok.Location = new System.Drawing.Point(178, 134);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(75, 23);
+            this.ok.TabIndex = 3;
+            this.ok.Text = "ok";
+            this.ok.UseVisualStyleBackColor = true;
+            // 
+            // cancelar
+            // 
+            this.cancelar.Location = new System.Drawing.Point(289, 134);
+            this.cancelar.Name = "cancelar";
+            this.cancelar.Size = new System.Drawing.Size(75, 23);
+            this.cancelar.TabIndex = 4;
+            this.cancelar.Text = "Cancelar";
+            this.cancelar.UseVisualStyleBackColor = true;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(440, 164);
+            this.ClientSize = new System.Drawing.Size(392, 169);
+            this.Controls.Add(this.cancelar);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.usuario);
+            this.Controls.Add(this.senha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sistema Controle Gerencial";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox senha;
+        private System.Windows.Forms.ComboBox usuario;
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Button cancelar;
     }
 }
 
